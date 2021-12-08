0adc929aade35f1e27ed9af830a6a5507adade3f
LasShukai <94955523+LasShukai@users.noreply.github.com>
2021-12-08 15:43:27 +0900

    Add files via upload

---
+++ js/success.js
@@ -0,0 +1,5 @@
+<div class="w-form-done" action="/#">
+          <div class="text-block">お問合せが送信しました!</div>
+          <div data-w-id="008b8981-e531-a01d-4baf-8c6b68b3d1f3" data-animation-type="lottie" data-src="documents/success.json" data-loop="0" data-direction="1" data-autoplay="1" data-is-ix2-target="0" data-renderer="svg" data-default-duration="3" data-duration="0"></div>
+          <div>お問合せいただき、ありがとうございます！</div>
+</div>
